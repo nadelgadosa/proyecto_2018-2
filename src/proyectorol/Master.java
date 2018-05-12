0c8005e53fa1c7971e02e3d456d0c703404326af
Usuario <Usuario@DESKTOP-2BBT55H>
2018-05-12 16:57:18 -0500

    metodos de lectura y consulta en master y jugador

--- src/proyectorol/Master.java
+++ src/proyectorol/Master.java
@@ -1,6 +1,17 @@
 package proyectorol;
 
-public class Master extends Usuario {
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Master extends Usuario {   
   private int juegos, permisos;
   private Juego[] juego = new Juego[3];
 
@@ -25,12 +36,90 @@ public class Master extends Usuario {
         super(null, null);
     }
   
-       public Master(int juegos, int permisos, String nombreUsusario, String clave) {
+    public Master(int juegos, int permisos, String nombreUsusario, String clave) {
         super(nombreUsusario, clave);
         this.juegos = juegos;
         this.permisos = permisos;
     }
-       
+     
+    
+    
+    public Master BuscarUsuario(String user){
+        
+        Master master = new Master();
+        try{
+            
+            final Path archivoSer = new File("C:\\Users\\Usuario\\Documents\\NetBeansProjects\\proyectoRol\\data\\Master\\"+user+".ser").toPath();
+            final ObjectInputStream leerUsuario = new ObjectInputStream(Files.newInputStream(archivoSer));
+            master = (Master)leerUsuario.readObject();
+            leerUsuario.close();
+            
+        }catch(Exception e){
+            
+        }
+        return master;
+    }
+    
+    public boolean verificarIngreso(Master master, String clave){
+        boolean aux;
+        if(master.getClave()== clave){
+            aux = true;
+        }else{
+            aux = false;
+        }
+        
+        
+        return aux;
+    }
+    
+    public String [] listaDeJuegos(String user){
+        String [] juegos = null;
+        int i=0;
+         try {
+            File inFile = new File("C:\\Users\\Usuario\\Documents\\NetBeansProjects\\proyectoRol\\data\\Master\\"+user+".txt");
+            FileReader fR = new FileReader(inFile);
+            Scanner in = new Scanner(inFile);      
+            while(in.hasNext()){
+                 
+                 juegos [i] = in.next();
+                 i++;
+                 
+            }
+            in.close();           
+        } catch (FileNotFoundException ex) {
+             System.out.println(ex);
+        } 
+        return juegos;
+    }
+    
+    public Juego[] listaDeObjetosJuegos (String [] nombresDeArchivos){
+        int i = nombresDeArchivos.length;
+        Juego[] objetos = new Juego[i];
+        for (int j = 0; j <= i ; j++) {
+            try{
+            
+            final Path archivoSer = new File("C:\\Users\\Usuario\\Documents\\NetBeansProjects\\proyectoRol\\data\\Master\\"+nombresDeArchivos[j]+".ser").toPath();
+            final ObjectInputStream leerUsuario = new ObjectInputStream(Files.newInputStream(archivoSer));
+            objetos [j] = (Juego)leerUsuario.readObject();
+            leerUsuario.close();
+            
+        }catch(Exception e){
+                System.out.println(e);   
+        }
+        }
+        return objetos ;
+    }
+    
+    public String Juego(Juego [] juego, int i){
+        if(juego[i]==null){
+          return "new";  
+        }else{
+       return juego[i].toString(); 
+    }    
+    }
+   
+    
+    
     public int getJuegos() {
         return juegos;
     }
